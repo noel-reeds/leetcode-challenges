79f7f2f5a9226e7b25543c077c9baec6b5186ed7
altoids <noelreeds01@gmail.com>
2025-07-10 11:40:46 +0300

    contains Python solutions

---
+++ python_solutions/palindrome.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    """Soln Class for Palindrome task"""
+
+    def isPalindrome(self, x: int) -> bool:
+        """Returns True for a palindrome, otherwise False"""
+        pass
