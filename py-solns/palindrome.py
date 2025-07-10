a37c59519586ecc549ce9156851bb004cb95f433
altoids <noelreeds01@gmail.com>
2025-07-10 11:43:08 +0300

    renamed dir

--- py-solns/palindrome.py
+++
@@ -1,6 +0,0 @@
-class Solution(object):
-    """Soln Class for Palindrome task"""
-
-    def isPalindrome(self, x: int) -> bool:
-        """Returns True for a palindrome, otherwise False"""
-        pass
